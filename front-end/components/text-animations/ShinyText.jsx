e6786f94e39144c670d35826e002f3aff7cbf9e4
Shivam Goswami <shivamg1008@gmail.com>
2025-04-17 12:33:25 +0530

    Added text-animations

---
+++ front-end/components/text-animations/ShinyText.jsx
@@ -0,0 +1,44 @@
+// -------------Use component in page-----------
+
+
+// import ShinyText from './ShinyText';  
+// <ShinyText text="Just some shiny text!" disabled={false} speed={3} className='custom-class' />
+
+
+const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
+    const animationDuration = `${speed}s`;
+  
+    return (
+      <div
+        className={`text-[#b5b5b5a4] bg-clip-text inline-block ${disabled ? '' : 'animate-shine'} ${className}`}
+        style={{
+          backgroundImage: 'linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
+          backgroundSize: '200% 100%',
+          WebkitBackgroundClip: 'text',
+          animationDuration: animationDuration,
+        }}
+      >
+        {text}
+      </div>
+    );
+  };
+  
+  export default ShinyText;
+  
+  //------- config your tailwind.config.js------------
+  // module.exports = {
+  //   theme: {
+  //     extend: {
+  //       keyframes: {
+  //         shine: {
+  //           '0%': { 'background-position': '100%' },
+  //           '100%': { 'background-position': '-100%' },
+  //         },
+  //       },
+  //       animation: {
+  //         shine: 'shine 5s linear infinite',
+  //       },
+  //     },
+  //   },
+  //   plugins: [],
+  // };
